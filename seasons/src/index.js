1b0733b21867a3043a2b3fe08d9e933e2250de29
Justin Chan <jchan922@gmail.com>
2018-11-26 17:28:09 -0800

    completed Season Display app

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+    constructor (props) {
+        
+        super(props);
+        this.state = {
+            errorMessage: '',
+            latitude: null,
+            longitude: null
+        }
+
+    }
+
+    componentDidMount () {
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                })
+            },
+            (err) => {
+                this.setState({
+                    errorMessage: err.message
+                })
+            }
+        );
+
+    }
+
+    renderContent () {
+
+        if (this.state.latitude && !this.state.errorMessage) {
+            return ( 
+                <SeasonDisplay
+                    latitude={this.state.latitude}
+                    longitude={this.state.longitude}
+                />
+            )
+        }
+
+        if (!this.state.latitude && this.state.errorMessage) {
+            return (   
+                <div>Error: {this.state.errorMessage}</div>
+            )
+        }
+
+        return <Spinner message="Please accept location request"  />
+
+    }
+
+    render () {
+
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+
+    }
+
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
